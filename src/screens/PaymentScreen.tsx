ed07b9e2d0da8c29fae80b60315ba592d7615ed6
Hassan Ali <122424731+Hassanali477@users.noreply.github.com>
2024-01-10 14:55:25 +0500

    Home Screen Completed

---
+++ src/screens/PaymentScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const PaymentScreen = () => {
+  return (
+    <View>
+      <Text>PaymentsScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default PaymentScreen;
---
+++ src/store/store.ts
@@ -0,0 +1,38 @@
+import {create} from 'zustand';
+import {produce} from 'immer';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CoffeeData from '../data/CoffeeData';
+import BeansData from '../data/BeansData';
+
+export const useStore = create(
+  persist(
+    (set, get) => ({
+      CoffeeList: CoffeeData,
+      BeanList: BeansData,
+      CartPrice: 0,
+      FavoriteList: [],
+      CartList: [],
+      OrderHistoryList: [],
+      addToCart: (cartItem: any) =>
+        set(
+          produce(state => {
+            let found = false;
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == cartItem.id) {
+                found = true;
+                let size = false
+                for(let j=0; j<state.CartList[i].prices.length; j++){
+                  
+                }
+              }
+            }
+          }),
+        ),
+    }),
+    {
+      name: 'coffee-app',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
